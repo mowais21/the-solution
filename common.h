9691ee39b5efe6c0475d2629e9c968749bda7d8f
NyleD <nyle.dharani@gmail.com>
2019-02-18 16:50:31 -0500

    msg

--- common.h
+++ common.h
@@ -7,3 +7,24 @@ class Output {
 
 
 };
+
+class Passenger {
+  int ID;
+
+  int start_row;
+  int start_col;
+  int end_row;
+  int end_col;
+  int start_time;
+  int end_time;
+
+};
+
+class Cab {
+  vector<int>  history;
+
+  int row;
+  int col;
+  int ID; // starts from 0;
+
+};
